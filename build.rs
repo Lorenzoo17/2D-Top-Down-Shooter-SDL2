b2f2dab470a607b49a61aa866ecadd12425601b2
Lorenzo Mascia <138528390+Lorenzoo17@users.noreply.github.com>
2025-04-23 21:47:53 +0200

    Aggiornamento path build.rs, messo relativo

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=C:/Users/loren/Documents/Progetti rust/tpd_shooter_sdl/libs/SDL2/lib/x64"); // o lib/x86 se compili a 32 bit
+    println!("cargo:rustc-link-search=native=./libs/SDL2/lib/x64"); // o lib/x86 se compili a 32 bit
     println!("cargo:rustc-link-lib=SDL2");
     println!("cargo:rustc-link-lib=SDL2_ttf");
     println!("cargo:rustc-link-lib=SDL2_image");
